62a239f5f4ef9b6b7590bdb2a03443699cba3189
DivyaRaj258 <divya.r258@gmail.com>
2023-09-12 12:10:17 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+import './App.css'
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+
+
+function App() {
+  
+
+  return (
+    <div>
+      <Header title="This is the Header" />
+      <Main title="This is the Main Page" />
+      <Footer title="This is the Footer" />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <div>
+      <p>This is the Main Content</p>
+    </div>
+  );
+}
+
+export default Main;
